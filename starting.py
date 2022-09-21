8b58ffc5967ef99e7803e4dd2a71e2ca1687b937
Saiful Rahman <52599620+Saiful234@users.noreply.github.com>
2022-09-21 23:27:19 +0530

    Add files via upload

--- starting.py
+++ starting.py
@@ -2,18 +2,21 @@ from amount import Flask
 
 app = Flask(__name__)
 
-
 @app.route('/')
 def hello_world():
     return '<h1>Hello, World!</h1>'
 
 @app.route('/bye')
 def bye():
-    return "<h2>Bye</h2>"
+    return "<h2>Bye</h2>"\
 
-@app.route("/<int:number>/<name>")
-def greet(name, number):
-    return f"Hello{name}, You are {number} years old."
+@app.route("/username/<name>")
+def greet(name, age):
+    return f"Hello {name}!, You are {age} years old."
+
+# @app.route("/<int:post_id>")
+# def post(post_id):
+#     return f"Post {post_id}"
 
 if __name__ == "__main__":
     app.run(debug=True)
