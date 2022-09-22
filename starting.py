3c621132d62b69301eabaa12fd15bcb175238243
Saiful Rahman <52599620+Saiful234@users.noreply.github.com>
2022-09-22 23:52:00 +0530

    Add files via upload

--- starting.py
+++ starting.py
@@ -4,19 +4,20 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Hello, World!</h1>'
+    return '<h1 style="text-align: center">Hello, World!</h1>' \
+           '<img scr="https://media.giphy.com/media/coxQHKASG60HrHtvkt/giphy.gif">'
 
 @app.route('/bye')
 def bye():
-    return "<h2>Bye</h2>"\
+    return "<h2>Bye</h2>"
 
 @app.route("/username/<name>")
-def greet(name, age):
-    return f"Hello {name}!, You are {age} years old."
+def greet(name):
+    return f"Hello {name}!, You are years old."
 
-# @app.route("/<int:post_id>")
-# def post(post_id):
-#     return f"Post {post_id}"
+@app.route("/<int:post_id>")
+def post(post_id):
+    return f"Post {post_id}"
 
 if __name__ == "__main__":
     app.run(debug=True)
