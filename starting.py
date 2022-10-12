6a00647769b7da13df3f64b66002ddb25f5decc7
Saiful Rahman <52599620+Saiful234@users.noreply.github.com>
2022-10-12 23:07:27 +0530

    Create starting.py

--- starting.py
+++ starting.py
@@ -33,14 +33,3 @@ def hello_world():
 @make_underlined
 def bye():
     return "Bye!"
-
-
-#Creating variable paths and converting the path to a specified data type
-@app.route("/username/<name>/<int:number>")
-def greet(name, number):
-    return f"Hello there {name}, you are {number} years old!"
-
-
-if __name__ == "__main__":
-    #Run the app in debug mode to auto-reload
-    app.run(debug=True)
