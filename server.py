c775432f16af3361ea7c4845ff418fc0975fb956
Saiful Rahman <52599620+Saiful234@users.noreply.github.com>
2022-10-20 23:40:24 +0530

    Add files via upload

--- server.py
+++ server.py
@@ -1,19 +1,24 @@
 from flask import Flask
 import random
-
+random_number = random.randint(0, 9)
 app = Flask(__name__)
 
 @app.route('/')
 def input_guess():
-    return '<h1> Guess a number between 0 and 9</h1>'
+    return "<h1> Guess a number between 0 and 9</h1>"\
+           "<img src='https://media.giphy.com/media/3o7aCSPqXE5C6T8tBC/giphy.gif'/>"
 
 @app.route('/<int:guess_no>')
 def random_guess(guess_no):
-    random_number = random.randint(0, 9)
-    if guess_no == random_number:
-        return f"You got it right, You guessed{guess_no} and the answer is {random_number}"
+    if guess_no > random_number:
+        return "<h1 style='color: purple'>Too high, try again! </h1>"\
+               "<img src='https://media.giphy.com/media/3o6ZtaO9BZHcOjmErm/giphy.gif'/>"
+    elif guess_no < random_number:
+        return "<h1 style='color: red'>Too low, try again!</h1>"\
+               "<img src='https://media.giphy.com/media/jD4DwBtqPXRXa/giphy.gif'/>"
     else:
-        return f"Try again!, You guessed{guess_no} and answer is {random_number}"
+        return '<h1>You found me!</h1>'\
+               "<img src='https://media.giphy.com/media/4T7e4DmcrP9du/giphy.gif'/>"
 
 if __name__ == "__main__":
     app.run(debug=True)
