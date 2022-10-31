a461ff9daef8fab24b53f9c21d9c4ebee263fba2
Saiful Rahman <52599620+Saiful234@users.noreply.github.com>
2022-10-31 21:45:40 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -3,10 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return render_template('index.html')
-
-print(Flask(__name__))
+def hello():
+    return render_template('card.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
